3352e00b5195b48303f0b6ad80c1d6920a55b692
K.H <k.hartogs@sintlucasedu.nl>
2023-05-31 12:52:04 +0200

    beter

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,6 @@ function openModal(id) {
     document.body.classList.add('jw-modal-open');
 }
 
-
 function closeModal() {
     document.querySelector('.jw-modal.open').classList.remove('open');
     document.body.classList.remove('jw-modal-open');
