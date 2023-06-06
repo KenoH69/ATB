3d68cc71efc2ca97ec0035bb7535e87c124bdf37
K.H <k.hartogs@sintlucasedu.nl>
2023-06-06 22:43:32 +0200

    huge

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,14 @@
 
 function openModal(id) {
     document.getElementById(id).classList.add('open');
+    document.getElementById('map').className = '';
+    document.getElementById('map').classList.add('open-'+id);
     document.body.classList.add('jw-modal-open');
 }
 
 function closeModal() {
     document.querySelector('.jw-modal.open').classList.remove('open');
+    document.getElementById('map').className = '';
     document.body.classList.remove('jw-modal-open');
 }
 
